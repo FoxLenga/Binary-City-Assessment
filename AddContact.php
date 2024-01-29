27e55210bab084d8db48360dd722903cb0ce76a1
PaulusH <foxhauwanga@gmail.com>
2024-01-29 22:24:22 +0200

    Update AddContact.php

--- AddContact.php
+++ AddContact.php
@@ -1,89 +1,77 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "binary_city";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch clients from the database ordered by name ascending
+$sql = "SELECT * FROM contacts ORDER BY name ASC";
+$result = $conn->query($sql);
+
+// Close the database connection
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>List of Clients</title>
+   
 </head>
 <body>
-<?php
+<header>
+        <nav>
+            <ul>
+                <li><a href="GetClients.php">Clients</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    class ContactManager
-    {
-        private $conn;
-    
-        // Constructor to establish database connection
-        public function __construct($servername , $username, $password, $dbname)
-        {
-            $this->conn = new mysqli($servername, $username, $password, $dbname);
-    
-            if ($this->conn->connect_error) {
-                die("Connection failed: " . $this->conn->connect_error);
+<h2>List of Clients</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email Address</th>
+            <th>Linked Client</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        // Display the list of clients in a table
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                // echo "<td>" . $row["name"] . "</td>";
+                echo "<td>" . $row["surname"] . " " . $row["name"] . "</td>";
+                echo "<td>" . $row["email address"] . "</td>";
+                echo "<td>" . $row["link clients"] . "</td>";
+                echo "</tr>";
             }
+        } else {
+            echo "<tr><td colspan='4'>No Contact (s) found</td></tr>";
         }
-    
-        // Fetch clients from the database ordered by name ascending
-        public function getClients()
-        {
-            $sql = "SELECT * FROM clients ORDER BY contacts ASC";
-            $result = $this->conn->query($sql);
-    
-            // Check if there are clients
-            if ($result->num_rows > 0) {
-                // Return the list of clients as an associative array
-                return $result->fetch_all(MYSQLI_ASSOC);
-            } else {
-                // Return 'No clients found' message
-                return "No contact (s) found";
-            }
-        }
-    
-        // Destructor to close the database connection
-        public function __destruct()
-        {
-            $this->conn->close();
-        }
-    }
-    
-    // Database connection details
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Binary_City";
-    
-    // Create an instance of the ContactsManager class
-    $ContactManager = new ContactManager($servername, $username, $password, $dbname);
-    
-    // Get the list of clients
-    $contacts = $ContactManager->getClients();
-    
-    // Display the list or the message
-    if (is_array($contacts)) {
-        echo "<h2>List of Clients</h2>";
-        echo "<ul>";
-        foreach ($contacts as $contact) {
-            echo "<li>" . $contact["name"] . " - " . $contact["Surname"] . " - " . $contact["Email Address"]. "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo $clients;
-    }
-    
-   
-    
+        ?>
+    </tbody>
+</table>
 
-?>
- <form method="post">
-  <label for="contactname">Name:</label><br>
-  <input type="text" id="contactname" name="contactname"><br>
-
-  <label for="surname">Surname:</label><br>
-  <input type="text" id="surname" name="surname"><br>
-
-  <label for="email">Email: </label><br>
-  <input type="email" id="email" name="email"><br>
- 
-  <input type="submit" id="submit" name="submit">
-</form> 
+<div>
+        <a href="AddContact.php" target="_blank" class="addContactBtn">Add Contact</a>
+    </div>
 </body>
 </html>
